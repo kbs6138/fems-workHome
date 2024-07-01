91977ec100da08a0ed40be321c85720d71259896
kbs6138@naver.com <kbs6138@naver.com>
2024-07-01 14:52:05 +0900

    차트 넣음

--- src/Components/Charts/Thermometer.jsx
+++ src/Components/Charts/Thermometer.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+// ThermometerComponent.js
+import React, { useContext } from "react";
 import Thermometer from "react-thermometer-component";
+import { ThemeContext } from "../ThemeContext";
 
 export default function ThermometerComponent() {
+  const { isDarkMode } = useContext(ThemeContext);
+
   return (
     <div className="Thermometer" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70px', marginTop:'100px' }}>
       <Thermometer
-        theme="light"
+        theme={isDarkMode ? "dark" : "light"}
         value={75}
         max={100}
         size="small"
-        height={130}
+        height={95}
         steps={2}
         reverseGradient={true}
+        format="°C"
       />
     </div>
   );
